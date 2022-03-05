e5fcb7e5194d75f05e0439515474447af87750c4
상윤 <yoon@192.168.nate.com>
2022-03-05 16:53:37 +0900

    당근마켓 첫  커밋

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.download_wrapper > label').click(function (e) {
+        // 화살표를 180도 회전한다
+        $('.menu-icon-svg').toggleClass('open');
+        // 리스트를 표시한다
+        $('.download_wrapper > ul').toggleClass('open');
+    });
+
+    // 아이템 컨테이너 복사하기
+    for (var i = 0; i < 3; i++) {
+        var card = $("div.items").clone();
+        card.appendTo(".items_container");
+    }
+
+    // AOS 사용하기
+    AOS.init();
+
+    // 상단 메뉴 고정소스
+    var offset = $("#header_wrap").offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > offset.top) {
+            $("#header_wrap").addClass("fixed");
+        }
+        else {
+            $("#header_wrap").removeClass("fixed");
+        }
+    });
+});
